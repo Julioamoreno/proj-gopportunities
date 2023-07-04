ec6a5fdd5f938a070a28b1c77b41c2e7cdf4fe6c
julioamoreno <julioarmando321@gmail.com>
2023-07-04 16:33:27 -0300

    feat: add opening routes

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Julioamoreno/proj-gopportunities/router"
+)
+
+func main() {
+	router.Initialize()
+}
---
+++ router/router.go
@@ -0,0 +1,11 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func Initialize() {
+	router := gin.Default()
+
+	initializeRoutes(router)
+
+	router.Run() // listen and serve on 0.0.0.0:8080
+}
