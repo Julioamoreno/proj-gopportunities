e852075418f2ae437295e41fba9d8d0298feaa31
julioamoreno <julioarmando321@gmail.com>
2023-07-06 18:40:06 -0300

    feat: add request and response helpers

---
+++ handler/response.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sendError(ctx *gin.Context, code int, msg string) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message":   msg,
+		"errorCode": code,
+	})
+}
+
+func sendSuccess(ctx *gin.Context, data interface{}) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(http.StatusOK, data)
+}
