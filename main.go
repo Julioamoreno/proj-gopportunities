0b939b3de8c4363e8ad8c3951cc42d80515c2e4e
julioamoreno <julioarmando321@gmail.com>
2023-07-06 11:17:22 -0300

    feat: add logger service

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"github.com/Julioamoreno/proj-gopportunities/config"
 	"github.com/Julioamoreno/proj-gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.ErrorF("Config initialization error: %v", err)
+
+		return
+	}
+
 	router.Initialize()
 }
